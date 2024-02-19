7cddf89b03f42288b78129d25f76b560accc4477
Adam Khodier <khodieradam1297@gmail.com>
2024-02-18 23:10:50 -0500

    solved

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // Problem: Given an integer 'n', find the factorial of that integer.
 // The factorial of a non-negative integer 'n', denoted m!, is the product of all positive integers <= to 'n'.
+// Factorial of 0 and 1 is === 1.
+
+function factorial(n) {
+    let result = 1
+    for(let i = 2; i <= n; i++) {
+        result = result * i
+    }
+    return result
+}
+
+console.log(factorial(5))
+console.log(factorial(10))
+console.log(factorial(20))
